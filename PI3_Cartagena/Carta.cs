b2271c39dedf4d8fdb343d838b7ce2a33c9aaf6e
Paulo Sergio Lourenço <ygorguito16@gmail.com>
2023-03-26 10:59:48 -0300

    Listar Cartas com imagem

--- PI3_Cartagena/Carta.cs
+++ PI3_Cartagena/Carta.cs
@@ -24,21 +24,138 @@ namespace PI3_Cartagena
             int id = Convert.ToInt32(txt_UserId.Text);
             string senha = txt_SenhaUser.Text;
 
-
-
             string cartas = Jogo.ConsultarMao(id, senha);
 
             cartas = cartas.Replace("\r", "");
             string[] mao = cartas.Split('\n');
-
-
-            
-
+            List<int> qtd = new List<int>();
+            List<string> icone = new List<string>();
 
             for (int i = 0; i < mao.Length; i++)
             {
                 lb_cartas.Items.Add(mao[i]);
             }
+            for (int i = 0; i < mao.Length; i++)
+            {
+                if (mao[i].Length >= 3)
+                {
+                    qtd.Add(Convert.ToInt32(mao[i].Substring(2)));
+                    icone.Add(mao[i].Substring(0, mao[i].Length - 2));
+                }
+            }
+            int contador = 1;
+            int posQtd = 0;
+            // "C", "T", "E", "F", "G", "P"
+            foreach (var item in icone)
+            {
+                switch (item)
+                {
+                    case "C":
+                        for (int j = 0; j < qtd[posQtd]; j++)
+                        {
+                            var picture = new PictureBox
+                            {
+                                Name = "pictureBox",
+                                Size = new Size(40, 50),
+                                Location = new Point(550, 60),
+                                SizeMode = PictureBoxSizeMode.StretchImage,
+                                Image = Image.FromFile("Icones/Chave.png"),
+                            };
+                            this.Controls.Add(picture);
+                            contador++;
+
+                        }
+                        posQtd++;
+                        break;
+                    case "T":
+                        for (int j = 0; j < qtd[posQtd]; j++)
+                        {
+                            var picture = new PictureBox
+                            {
+                                Name = "pictureBox",
+                                Size = new Size(40, 50),
+                                Location = new Point(550, 60 * contador),
+                                SizeMode = PictureBoxSizeMode.StretchImage,
+                                Image = Image.FromFile("Icones/Tricornio.jpg"),
+                            };
+                            this.Controls.Add(picture);
+                            contador++;
+
+                        }
+                        posQtd++;
+                        break;
+
+                    case "E":
+                        for (int j = 0; j < qtd[posQtd]; j++)
+                        {
+                            var picture = new PictureBox
+                            {
+                                Name = "pictureBox",
+                                Size = new Size(40, 50),
+                                Location = new Point(550, 60 * contador),
+                                SizeMode = PictureBoxSizeMode.StretchImage,
+                                Image = Image.FromFile("Icones/Esqueleto.jpg"),
+                            };
+                            this.Controls.Add(picture);
+                            contador++;
+
+                        }
+                        posQtd++;
+                        break;
+                    case "F":
+                        for (int j = 0; j < qtd[posQtd]; j++)
+                        {
+                            var picture = new PictureBox
+                            {
+                                Name = "pictureBox",
+                                Size = new Size(40, 50),
+                                Location = new Point(550, 60 * contador),
+                                SizeMode = PictureBoxSizeMode.StretchImage,
+                                Image = Image.FromFile("Icones/Faca.jpg"),
+                            };
+                            this.Controls.Add(picture);
+                            contador++;
+
+                        }
+                        posQtd++;
+                        break;
+                    case "G":
+                        for (int j = 0; j < qtd[posQtd]; j++)
+                        {
+                            var picture = new PictureBox
+                            {
+                                Name = "pictureBox",
+                                Size = new Size(40, 50),
+                                Location = new Point(550, 60 * contador),
+                                SizeMode = PictureBoxSizeMode.StretchImage,
+                                Image = Image.FromFile("Icones/Garrafa.jpg"),
+
+                            };
+                            this.Controls.Add(picture);
+                            contador++;
+
+                        }
+                        posQtd++;
+                        break;
+                    case "P":
+                        for (int j = 0; j < qtd[posQtd]; j++)
+                        {
+                            var picture = new PictureBox
+                            {
+                                Name = "pictureBox",
+                                Size = new Size(40, 50),
+                                Location = new Point(550, 60 * contador),
+                                SizeMode = PictureBoxSizeMode.StretchImage,
+                                Image = Image.FromFile("Icones/Pistola.jpg"),
+                            };
+                            this.Controls.Add(picture);
+                            contador++;
+                        }
+                        posQtd++;
+                        break;
+                }
+            }
+
         }
 
         private void partidaToolStripMenuItem1_Click(object sender, EventArgs e)
